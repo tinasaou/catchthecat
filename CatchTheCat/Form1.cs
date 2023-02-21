b10bce4167c622c32b4dc17d6491411e26a9dcd7
tinasaou <123185212+tinasaou@users.noreply.github.com>
2023-02-21 09:26:03 +0200

    Add files via upload

---
+++ CatchTheCat/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CatchTheCat
+{
+    
+    public partial class Form1 : Form
+    {
+        OleDbConnection connection;
+        //string connectionstring = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Game_user.mdb";
+        String connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Database1.mdb;";
+   
+  
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Form2 form2 = new Form2();
+            form2.Show();
+            this.Hide();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connection = new OleDbConnection(connectionString);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            StringBuilder sb = new StringBuilder();
+            String query = "Select score from Game where name ='" + textBox1.Text + "';";
+            OleDbCommand cmd = connection.CreateCommand();
+            cmd.CommandText = query;
+            OleDbDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    string aaa = reader.GetString(0);
+                    sb.Append(Environment.NewLine);
+                    MessageBox.Show("Hello Akis your score is " + aaa);
+                }
+            }
+            else
+            {
+                MessageBox.Show("new Player");
+                insert();
+                
+                            }
+            connection.Close();
+        }
+        /*public void insert() { 
+            connection.Open();
+            String name = textBox1.Text;
+            OleDbCommand command = connection.CreateCommand();
+            //StringBuilder sb2 = new StringBuilder();
+            String query = "Insert into Game([name]) values(@name)";
+            command.CommandText = query;
+            command.Parameters.AddRange(new OleDbParameter[] {
+            new OleDbParameter("name", name)
+            }); ;
+            connection.Close();
+        }
+        */
+        public void insert()
+        {
+            string name = textBox1.Text;
+            string query = "INSERT INTO Game ([name]) VALUES (@name)";
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                using (OleDbCommand command = new OleDbCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@name", name);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ CatchTheCat/Form2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CatchTheCat
+{
+    public partial class Form2: Form
+    {
+
+        static int score = 0;
+        int x;
+        int y;
+        Random rand = new Random();
+        //List<PictureBox> list = new List<PictureBox>();
+
+        public Form2()
+        {
+
+            InitializeComponent();
+            timer2.Tick += delegate {
+                MessageBox.Show("user scored "+score);
+                this.Close();
+            };
+            timer2.Interval = (int)TimeSpan.FromMinutes(1.5).TotalMilliseconds;
+            timer2.Start();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            score++;
+            if (score == 10)
+            {
+                timer1.Stop();
+                timer1.Interval = 700;
+                timer1.Start();
+                
+            }
+          
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            x = rand.Next(0, this.Width - pictureBox1.Width);
+            y = rand.Next(0, this.Height - pictureBox1.Height);
+            this.pictureBox1.Location = new System.Drawing.Point(x, y);
+            Console.WriteLine("score = " + score);
+            
+            
+        }
+
+        
+
+        
+    }
+}
